e2e2e20e10c71ab17f328fb69165b5f9f93d7ef3
N8Nuks <nate.nuks@me.com>
2026-06-27 14:19:38 +0000

    Remove logo name from footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,6 @@ export default function Footer() {
   return (
     <footer className="border-t border-white/5 px-12 py-8 bg-black">
       <div className="mx-auto max-w-6xl flex flex-col sm:flex-row items-center justify-between gap-4">
-        <span className="text-sm text-white/30">Black Diamond Labs</span>
         <div className="flex items-center gap-6 text-xs text-white/20">
           <a href="mailto:info@blackdiamondlabs.co.nz" className="hover:text-white/50 transition-colors">info@blackdiamondlabs.co.nz</a>
         </div>
